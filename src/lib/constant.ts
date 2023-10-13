4afac42989fbf1d5a0eccada933607d4f5dc333c
최은석 <trgf456@naver.com>
2023-10-13 20:42:57 +0900

    first commit

---
+++ src/lib/constant.ts
@@ -0,0 +1,61 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "mildduck",
+  description: "포트폴리오를 만들어보세요.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Create",
+      href: "/create",
+    },
+    {
+      label: "MyPage",
+      href: "/mypage",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
