024e6e4dcc94a72144711b0fe14c1f44e52d8711
TheON2 <sniperad@naver.com>
2023-10-15 16:59:58 +0900

    Update : apply grid layout

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,56 +5,101 @@ import Image from "next/image";
 
 const CreatePage = () => {
   return (
-    <div className="h-screen grid grid-cols-10 p-24 bg-white shadow-md text-gray-800">
-      {/* 왼쪽 2:8 비율의 컨테이너 */}
-      <div className="grid grid-rows-10 col-span-2 space-y-4">
-        <div className="row-span-3 flex flex-col justify-center items-center text-center">
+    <div className="h-full bg-white shadow-md text-gray-800">
+      {/* 왼쪽 2/10 비율의 컨테이너 */}
+      <div className="row-span-4 grid grid-cols-10">
+        <div className="col-span-3 flex flex-col justify-center items-center text-center">
           <Avatar
             src="https://i.pravatar.cc/150?u=a04258114e29026708c"
             className="w-30 h-30 text-large mb-4"
           />
           <h1 className="text-2xl font-bold mb-2">김도원</h1>
-          <p className="text-gray-600">프론트엔드</p>
+          <h2 className="text-gray-600 text-3xl font-bold">프론트엔드</h2>
         </div>
 
-        <div className="row-span-3">
-          <h2 className="text-xl font-bold border-b-4 pb-2">Tech Stack</h2>
-          {/* 기술 스택 내용 */}
-        </div>
-
-        <div className="row-span-1">
-          <h2 className="text-xl font-bold border-b-4 pb-2">학력</h2>
-          {/* 학력 내용 */}
-        </div>
-
-        <div className="row-span-3">
-          <h2 className="text-xl font-bold border-b-4 pb-2">개발 경력</h2>
-          {/* 개발 경력 내용 */}
-        </div>
-      </div>
-      <div className="col-span-1"></div>
-
-      {/* 오른쪽 8:10 비율의 컨텐츠 영역 */}
-      <div className="col-span-7 pl-8 grid grid-rows-10">
-        {/* 이메일 등의 정보 영역 - 4:10 비율 */}
-        <div className="row-span-2">
-          <ul className=" space-y-2 text-sm">
-            <li>Email | sniperead@naver.com</li>
-            <li>KAKAO | laufetv@gmail.com</li>
+        <div className="col-span-7 mr-16">
+          <ul
+            style={{ listStyleType: "disc", paddingLeft: "20px" }}
+            className="space-y-2 text-sm font-bold border-b-4 border-t-4 pb-2 m-10"
+          >
+            <li>
+              Email |{" "}
+              <a href="mailto:sniperead@naver.com">sniperead@naver.com</a>
+            </li>
+            <li>
+              KAKAO | <a href="mailto:laufetv@gmail.com">laufetv@gmail.com</a>
+            </li>
             <li>PHONE | 010-2623-0279</li>
-            <li>Github | https://github.com/TheON2</li>
-            <li>Blog | https://velog.io/@theon2</li>
+            <li>
+              Github |{" "}
+              <a
+                href="https://github.com/TheON2"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://github.com/TheON2
+              </a>
+            </li>
+            <li>
+              Blog |{" "}
+              <a
+                href="https://velog.io/@theon2"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://velog.io/@theon2
+              </a>
+            </li>
+          </ul>
+          <ul
+            style={{ listStyleType: "disc", paddingLeft: "20px" }}
+            className="space-y-2 text-sm font-bold border-b-4 pb-2 m-10"
+          >
+            <li>
+              꾸준히 학습하고 발전하는 것에 관심이 많은 주니어개발자 김도원
+              입니다.
+            </li>
+            <li>
+              NextJs 13과 TypeScript를 주력 기술로 활용하며, NodeJs 개발을 함께
+              공부하여 다양한 관점을 고려합니다.
+            </li>
+            <li>
+              꾸준한 깃허브 커밋과 블로그 작성을 통해 매일 새로운 것에 도전하고
+              기록합니다.
+            </li>
+            <li>
+              일어나면 가장 먼저 TIL 하루일과 작성과 개발커뮤니티 열람으로
+              하루를 시작합니다.
+            </li>
           </ul>
         </div>
+      </div>
 
-        {/* 여기에 3:10 비율의 컨텐츠를 추가 */}
-        <div className="row-span-3">{/* 내용을 여기에 추가해주세요 */}</div>
+      <div style={{ minHeight: "250px" }}>
+        <div className="grid grid-cols-10">
+          <div className="col-span-3">
+            <h2 className="text-xl font-bold pb-2 ml-10">Tech Stack</h2>
+          </div>
+          <div className="col-span-7"></div>
+        </div>
+      </div>
 
-        {/* 여기에 1:10 비율의 컨텐츠를 추가 */}
-        <div className="row-span-1">{/* 내용을 여기에 추가해주세요 */}</div>
+      <div style={{ minHeight: "150px" }}>
+        <div className="grid grid-cols-10">
+          <div className="col-span-3">
+            <h2 className="text-xl font-bold pb-2 ml-10">학력</h2>
+          </div>
+          <div className="col-span-7"></div>
+        </div>
+      </div>
 
-        {/* 여기에 2:10 비율의 컨텐츠를 추가 */}
-        <div className="row-span-2">{/* 내용을 여기에 추가해주세요 */}</div>
+      <div>
+        <div className="grid grid-cols-10">
+          <div className="col-span-3">
+            <h2 className="text-xl font-bold pb-2 ml-10">개발 경력</h2>
+          </div>
+          <div className="col-span-7"></div>
+        </div>
       </div>
     </div>
   );
